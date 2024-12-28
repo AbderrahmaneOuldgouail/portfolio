934252715d4711ae79e25c596ce27a2416ef6547
Abderrahmane Ouldgouail <abderrahmane.ouldgouail@gmail.com>
2024-12-28 17:07:15 +0100

    Add projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -2,6 +2,12 @@ import image from "@/app/[locale]/projects/hotel-sidielnoui/assets/img.png";
 import hotel_img1 from "@/app/[locale]/projects/hotel-sidielnoui/assets/booking.jpg";
 import hotel_img2 from "@/app/[locale]/projects/hotel-sidielnoui/assets/home_fr.jpg";
 
+import event_img1 from "@/app/[locale]/projects/event-plus/assets/home_screen.jpg";
+import event_img2 from "@/app/[locale]/projects/event-plus/assets/preference_screen.jpg";
+
+import agency_img1 from "@/app/[locale]/projects/soltanat-eldjamal/assets/home_section.jpg";
+import agency_img2 from "@/app/[locale]/projects/soltanat-eldjamal/assets/contact_section.jpg";
+
 export const projects = [
   {
     slug: "hotel-sidielnoui",
@@ -10,12 +16,19 @@ export const projects = [
     img1: hotel_img1,
     img2: hotel_img2,
   },
+  // {
+  //   slug: "event-plus",
+  //   github: "",
+  //   live: "",
+  //   img1: event_img1,
+  //   img2: event_img2,
+  // },
   {
-    slug: "event-plus",
-    github: "",
-    live: "",
-    img1: image,
-    img2: image,
+    slug: "soltanat-eldjamal",
+    github: "https://github.com/AbderrahmaneOuldgouail/soltanat-eldjamal",
+    live: "https://soltanat-eldjamal.vercel.app/en",
+    img1: agency_img1,
+    img2: agency_img2,
   },
 ];
 
